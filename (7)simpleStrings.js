494a5d9688e83223a6dffab041c086097b9ff2e4
Dan Price <danielparkerprice@gmail.com>
2020-10-10 21:01:00 +0100

    cw

---
+++ (7)simpleStrings.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5bc052f93f43de7054000188/train/javascript
+
+function reverseLetter(str) {
+  const regex = /[a-z]/ig
+  const result = str.match(regex).reverse().join('')
+  return result
+}
+
+console.log(reverseLetter('hello'))

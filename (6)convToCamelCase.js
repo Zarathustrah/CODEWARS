3ca7a0fcdefade8217bd589524a6effcda93f0db
Dan Price <danielparkerprice@gmail.com>
2020-12-06 22:25:34 +0000

    cw

--- (6)convToCamelCase.js
+++ (6)convToCamelCase.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/517abf86da9663f1d2000003/train/javascript
 
 function toCamelCase(str) {
-
+  return str
 }
 
-console.log(toCamelCase("the_stealth_warrior")))
+console.log(toCamelCase('the_stealth_warrior'))
